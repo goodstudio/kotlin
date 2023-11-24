0e41f43899f8ec4033034b4f53d1b4c05e22d610
Leonid Startsev <leonid.startsev@jetbrains.com>
2023-11-24 16:31:39 +0100

    fixup! Use type parameters' source information for kotlinx.serialization diagnostics

--- plugins/kotlinx-serialization/testData/diagnostics/NonSerializable.kt
+++ plugins/kotlinx-serialization/testData/diagnostics/NonSerializable.kt
@@ -13,6 +13,9 @@ class Basic(val foo: <!SERIALIZER_NOT_FOUND("NonSerializable")!>NonSerializable<
 class Inside(val foo: List<<!SERIALIZER_NOT_FOUND("NonSerializable")!>NonSerializable<!>>)
 
 @Serializable
+class Inside2(val foo: List<List<<!SERIALIZER_NOT_FOUND("NonSerializable")!>NonSerializable<!>>>)
+
+@Serializable
 class WithImplicitType {
     <!SERIALIZER_NOT_FOUND("NonSerializable")!>val foo = NonSerializable()<!>
 }
